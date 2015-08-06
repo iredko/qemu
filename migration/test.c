8a8ca4dc0dad5b62d6b40aefd4457657fdbc92c5
Igor Redko <redkoigor@gmail.com>
2015-08-06 15:05:06 +0300

    Now we using special hook. Slightly changed logic of estimator

--- migration/test.c
+++ migration/test.c
@@ -18,20 +18,21 @@ typedef struct QEMUFileTest {
 } QEMUFileTest;
 
 //stubs
-static int64_t test_result(void);
-
+static int64_t test_result(int64_t time_delta);
+static int64_t start_time;
+static bool migration_capability_compress;
+static bool zero_iteration_done = false;
 static uint64_t downtime;
 static uint64_t transfered_bytes = 0;
-static int64_t rounds = -1;
-static int64_t times = 0;
 static uint64_t initial_bytes = 0;
 static uint64_t dirtied_bytes;
+/*
 static int qemu_test_get_buffer(void *opaque, uint8_t *buf,
                                 int64_t pos, int size)
 {
     return -1;
 }
-
+*/
 static int qemu_test_put_buffer(void *opaque, const uint8_t *buf,
                                 int64_t pos, int size)
 {
@@ -42,87 +43,80 @@ static int qemu_test_put_buffer(void *opaque, const uint8_t *buf,
 
 static int qemu_test_close(void *opaque)
 {
+    MigrationState *s = opaque;
     qmp_migrate_set_downtime((double)(downtime)/1e9, NULL);
+    s->enabled_capabilities[MIGRATION_CAPABILITY_COMPRESS] = migration_capability_compress;
     return 0;
 }
-
+/*
 static int qemu_test_before_iterate(QEMUFile *f, void *opaque,
                                         uint64_t flags, void *data)
 {
     return 0;
 }
-
-static int64_t test_result(void)
+*/
+static int64_t test_result(int64_t time_delta)
 {
     double mbps = 1000;
-    int64_t time_delta = downtime/1000000;
     int64_t estimated_time_ms = 0;
     int64_t dt_ms;
     double Bpms = mbps * (1024 * 128 / 1000);
     double dirty_bytes_rate = dirtied_bytes/time_delta;
     uint64_t remaining = initial_bytes;
     uint64_t max_size = dirty_bytes_rate * (downtime / 1000000);
-    do{
-        dt_ms = remaining / Bpms;
-        remaining = dt_ms * dirty_bytes_rate;
-        estimated_time_ms += dt_ms;
-    }while (remaining > max_size);
-    dt_ms = remaining / (mbps * 8) * 1000;
-    estimated_time_ms += dt_ms;
+    trace_test_result ( initial_bytes, dirtied_bytes, dirty_bytes_rate, estimated_time_ms );
+    if(dirty_bytes_rate < Bpms){
+        do{
+            dt_ms = remaining / Bpms;
+            remaining = dt_ms * dirty_bytes_rate;
+            estimated_time_ms += dt_ms;
+        }while (remaining > max_size);
+        estimated_time_ms += remaining / Bpms;
+//TODO add working set estimation and comparing it with max_size
+    } else {
+        estimated_time_ms = -1;
+    }
     trace_test_result ( initial_bytes, dirtied_bytes, dirty_bytes_rate, estimated_time_ms );
     return estimated_time_ms;
 }
 static int qemu_test_sync_hook(QEMUFile *f, void *opaque,
                                         uint64_t flags, void *data)
 {
-    return 0;
+    int64_t end_time, time_delta;
+// if we got all information we should make our estimation and stop process
+    if (zero_iteration_done) {
+        end_time = qemu_clock_get_ns(QEMU_CLOCK_REALTIME);
+        if ( end_time - start_time < downtime ){
+            usleep( (downtime - (end_time - start_time))/ 1000);
+            time_delta = downtime / 1000000;
+        } else {
+            time_delta = (end_time - start_time) / 1000000;
+        }
+        dirtied_bytes = transfered_bytes - initial_bytes;
+        test_result(time_delta);
+        return -1;
+    } else {
+        start_time = qemu_clock_get_ns(QEMU_CLOCK_REALTIME);
+        zero_iteration_done = true;
+        initial_bytes = transfered_bytes;
+    }
+        return 0;
 }
-
+/*
 static int qemu_test_after_iterate(QEMUFile *f, void *opaque,
                                         uint64_t flags, void *data)
 {
-    MigrationState *s = opaque;
     switch ( flags ){
     case RAM_CONTROL_SETUP:
-        times++;
-        trace_probe_timestamp();
-        break;
     case RAM_CONTROL_ROUND:
-        if ( rounds == -1 ){
-            rounds = times * 2;
-        }
-        rounds--;
-        if ( (rounds % times) == 0 ){
-            if ( initial_bytes == 0 ){
-                initial_bytes = transfered_bytes;
-                transfered_bytes = 0;
-                usleep( downtime / 1000 );
-            } else {
-                dirtied_bytes = transfered_bytes;
-                test_result();
-            }
-        }
-        if ( rounds == 0 ) {
-            //push result UP
-           // migrate_set_state_wrap(s, MIGRATION_STATUS_ACTIVE,
-           //                           MIGRATION_STATUS_CANCELLING);
-            qemu_file_set_error (s->file, 1);
-            transfered_bytes = 0;
-            rounds = -1;
-            times = 0;
-            initial_bytes = 0;
-        }
         break;
 //WE SHOULD NEVER GET HERE!
     case RAM_CONTROL_FINISH:
         trace_probe_timestamp();
-        //migrate_set_state_wrap(s, MIGRATION_STATUS_ACTIVE,
-//                                      MIGRATION_STATUS_FAILED);
     }
-
     return 0;
 }
-
+*/
 static size_t qemu_test_save_page(QEMUFile *f, void *opaque,
                                   ram_addr_t block_offset, ram_addr_t offset,
                                   size_t size, uint64_t *bytes_sent)
@@ -134,25 +128,25 @@ static size_t qemu_test_save_page(QEMUFile *f, void *opaque,
 }
 
 //do not use for incoming
+/*
 static const QEMUFileOps test_read_ops = {
     .get_buffer         = qemu_test_get_buffer,
     .close              = qemu_test_close,
 };
-
+*/
 static const QEMUFileOps test_write_ops = {
     .hook_ram_sync      = qemu_test_sync_hook,
     .put_buffer         = qemu_test_put_buffer,
     .close              = qemu_test_close,
-    .before_ram_iterate = qemu_test_before_iterate,
-    .after_ram_iterate  = qemu_test_after_iterate,
     .save_page          = qemu_test_save_page,
 };
 
 static void *qemu_fopen_test(MigrationState *s, const char *mode)
 {
     QEMUFileTest *t;
-    downtime = migrate_max_downtime();
-    qmp_migrate_set_downtime(0, NULL);
+    transfered_bytes = 0;
+    initial_bytes = 0;
+    zero_iteration_done = false;
     if (qemu_file_mode_is_not_valid(mode)) {
         return NULL;
     }
@@ -174,7 +168,10 @@ static void *qemu_fopen_test(MigrationState *s, const char *mode)
 void test_start_migration(void *opaque, const char *host_port, Error **errp)
 {
     MigrationState *s = opaque;
+    downtime = migrate_max_downtime();
+    qmp_migrate_set_downtime(0, NULL);
     s->file = qemu_fopen_test(s, "wb");
+    migration_capability_compress = s->enabled_capabilities[MIGRATION_CAPABILITY_COMPRESS];
     s->enabled_capabilities[MIGRATION_CAPABILITY_COMPRESS] = false;
     migrate_fd_connect(s);
     return;
